a0a4e06c950bf7b060cae8ce7d0c618c867fb886
irinaisheree <irinastoyanova@abv.bg>
2024-02-02 12:38:25 +0200

    add registered user to db

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
 
+const authManager = require('../managers/authManager')
+
 router.get('/register', (req, res) => {
     res.render('auth/register')
+})
+
+router.post('/register', async(req, res) => {
+    const userData = req.body
+
+    await authManager.register(userData)
+
+    res.redirect('/auth/login')
 })
 
 module.exports = router
