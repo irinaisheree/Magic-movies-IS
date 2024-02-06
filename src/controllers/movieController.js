ac54320a1ff23e5d2936e7330b79a8a6be3de2a0
irinaisheree <irinastoyanova@abv.bg>
2024-02-06 09:46:46 +0200

    add dynamic links in the navigation

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -28,8 +28,8 @@ movieRouter.get('/movies/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId
     try{
         const movie = await movieManager.getOne(movieId).lean()
-        const isOwner = movie.owner == req.user._id//movie.owner(object) ==  req.user._id(string) (convirts them to the same type)
-    // const casts = await castManager.getByIds(movie.casts).lean()
+        const isOwner = movie.owner == req.user?._id//movie.owner(object) ==  req.user._id(string) (convirts them to the same type)
+        // const casts = await castManager.getByIds(movie.casts).lean() --- only if populate is not used(populates the cast info into the movie with the ref: Cast in the Movie Schema)
         res.render('movie/details', {movie, isOwner})
 } catch(error){
     console.log(error.message)
